8b444db947b7ae234d4c303f3273e80d41ce6505
Huw Taylor <Huw.Taylor@jbaconsulting.com>
2019-07-31 15:49:46 +0100

    added staging and unstaging; fixed hiding .vcr folder on win

--- vcr.rb
+++ vcr.rb
@@ -14,9 +14,9 @@
   - [X] List branches
   - [X] Switch branches
   - [ ] Delete branches
-  - [ ] Stage files
-  - [ ] List staged files
-  - [ ] Unstage files
+  - [X] Stage files
+  - [X] List staged files
+  - [X] Unstage files
   - [ ] Commit files
   - [ ] Push commits
   - [ ] List commits
@@ -61,6 +61,8 @@ require 'digest'
 require 'find'
 require 'fileutils'
 
+WINDOWS_PLATFORMS = ["bccwin", "cygwin", "djgpp", "mingw", "mswin"]
+
 def existing_repo?
     return File.exists?(root)
 end
@@ -76,22 +78,26 @@ def root
     return File.join(Dir.pwd, ".vcr")
 end
 
-def path(*args)
+def vcr_path(*args)
     return File.join(root, *args)
 end
 
+def repo_path(*args)
+    return File.join(Dir.pwd, *args)
+end
+
 def current_track
-    head = File.read(path("HEAD"))
+    head = File.read(vcr_path("HEAD"))
     if head.start_with? "ref: "
-        return path(head[5..-1]).sub(path("tracks"), "")[1..-1]
+        return head[5..-1].sub("tracks/", "")
     end
     return head
 end
 
 def current_frame
-    head = File.read(path("HEAD"))
+    head = File.read(vcr_path("HEAD"))
     if head.start_with? "ref: "
-        head = File.read(path(head[5..-1]))
+        head = File.read(vcr_path(head[5..-1]))
     end
     return head
 end
@@ -99,7 +105,7 @@ end
 def add_to_log(message)
     # Standardise an event log.
     # datetime, author, event type, message
-    File.open(path(".log"), 'a') do |f|
+    File.open(vcr_path(".log"), 'a') do |f|
         f.write(message + "\n")
     end
 end
@@ -110,22 +116,12 @@ def help(args)
         puts "VCR Help"
     else
         # Print help on command
-        puts "VCR Help about #{arg[0]}"
+        puts "VCR Help about #{args[0]}"
     end
 end
 
 def create_frame(frame_name)
-    source_path = Dir.pwd
-    target_path = path("frames", frame_name, ".frame")
-    Find.find(source_path) do |source|
-        target = source.sub(/^#{source_path}/, target_path)
-        if File.directory? source
-            Find.prune if File.basename(source) == '.vcr'
-            FileUtils.mkdir target unless File.exists? target
-        else
-            FileUtils.copy source, target
-        end
-    end
+    
 end
 
 def show_log(args)
@@ -140,6 +136,9 @@ def init(args)
     dir = File.join(args[0] || Dir.pwd, ".vcr")
 
     Dir.mkdir(dir)                       # vcr directory
+    if WINDOWS_PLATFORMS.any? { |platform| RUBY_PLATFORM.include?(platform) }
+        IO.popen(['attrib', '+H', dir])  # hide .vcr folder on windows
+    end
     Dir.mkdir(File.join(dir, "frames"))  # all snapshots
     Dir.mkdir(File.join(dir, "tracks"))  # all tracks
     Dir.mkdir(File.join(dir, "tags"))    # all tags
@@ -150,9 +149,8 @@ def init(args)
     File.write(File.join(dir, ".log"), "")   # log of all actions
 
     add_to_log(">>> init #{args[0] || Dir.pwd}")
-    add_to_log("VCR initialised.")
-    track([Actions::Tracks::CREATE, "master"])       # create master track
-    checkout([Actions::Context::CHECKOUT, "master"]) # checkout master track
+    track([Actions::Tracks::CREATE, "master"]) # create master track
+    checkout(["master"])                       # checkout master track
 end
 
 def track(args)
@@ -166,24 +164,24 @@ def track(args)
 
         # TODO: check if valid name
 
-        if File.exists?(path("tracks", track_name))
+        if File.exists?(vcr_path("tracks", track_name))
             puts "track already exists."
             add_to_log("track #{track_name} already exists")
             exit(1)
         end
 
-        head = File.read(path("HEAD"))
+        head = File.read(vcr_path("HEAD"))
         if head.start_with? "ref: "
-            head = File.read(path(head[5..-1]))
+            head = File.read(vcr_path(head[5..-1]))
         end
-        FileUtils.mkdir_p(File.dirname(path("tracks", track_name)))
-        File.write(path("tracks", track_name), head)
+        FileUtils.mkdir_p(File.dirname(vcr_path("tracks", track_name)))
+        File.write(vcr_path("tracks", track_name), head)
         add_to_log("created #{track_name} track at #{head}")
 
     when Actions::Tracks::LIST
-        Dir.glob(path("tracks", "**", "*")) do |item|
+        Dir.glob(vcr_path("tracks", "**", "*")) do |item|
             next if item == '.' or item == '..' or not File.file?(item)
-            track_name = item.sub(path("tracks"), "")[1..-1]
+            track_name = item.sub(vcr_path("tracks"), "")[1..-1]
             puts track_name
             add_to_log(track_name)
         end
@@ -201,6 +199,19 @@ end
 def add(args)
     ensure_vcr
 
+    args.each do |filename|
+        FileUtils.cp(repo_path(filename), vcr_path("staging", filename))
+        add_to_log(">>> stage #{filename}")
+    end
+end
+
+def remove(args)
+    ensure_vcr
+
+    args.each do |filename|
+        FileUtils.rm(vcr_path("staging", filename))
+        add_to_log(">>> unstage #{filename}")
+    end
 end
 
 def checkout(args)
@@ -210,17 +221,17 @@ def checkout(args)
     target = args[0]
     # TODO: check if valid target
 
-    if File.file?(path("tracks", target))
-        File.write(path("HEAD"), "ref: tracks/#{target}")
+    if File.file?(vcr_path("tracks", target))
+        File.write(vcr_path("HEAD"), "ref: tracks/#{target}")
         add_to_log("ref: tracks/#{target}")
 
-    elsif File.file?(path("tags", target))
-        File.write(path("HEAD"), "ref: tags/#{target}")
+    elsif File.file?(vcr_path("tags", target))
+        File.write(vcr_path("HEAD"), "ref: tags/#{target}")
         add_to_log("ref: tags/#{target}")
 
-    elsif Dir.entries(path("frames")).one? { |f| f.start_with? target }
-        frame = Dir.entries(path("frames")).find { |f| f.start_with? target }
-        File.write(path("HEAD"), frame)
+    elsif Dir.entries(vcr_path("frames")).one? { |f| f.start_with? target }
+        frame = Dir.entries(vcr_path("frames")).find { |f| f.start_with? target }
+        File.write(vcr_path("HEAD"), frame)
         add_to_log(frame)
 
     else
@@ -232,12 +243,45 @@ end
 
 def status(args)
     ensure_vcr
-    add_to_log(">>> status #{args.join(" ")}")
 
+    ignored_files = []
+    if File.file?(repo_path(".vcr-ignore"))
+        ignored_files = File.readlines(repo_path(".vcr-ignore")).map { |f| f.chomp }
+    end
 
-    puts "On track #{current_track
-    add_to_log(">>> status #{args.join(" ")}")
+    puts "On track #{current_track}"
+    puts
 
+    puts "Changes staged for commit:"
+    Dir.glob(vcr_path("staging", "**", "*")) do |item|
+        next if item == '.' or item == '..' or not File.file?(item)
+        file_name = item.sub(vcr_path("staging"), "")[1..-1]
+        # TODO: add colour here (green)
+        puts "\t" + file_name
+    end
+    puts
+
+    puts "Changes not staged for commit:"
+    Find.find(repo_path) do |item|
+        next if item == '.' or item == '..'
+        Find.prune if File.basename(item) == '.vcr-ignore'
+        Find.prune if File.basename(item) == '.vcr'
+        Find.prune if ignored_files.include?(File.basename(item))
+        if File.file?(item)
+            file_name = item.sub(repo_path, "")[1..-1]
+            staged_item = vcr_path("staging", file_name)
+            if File.file?(staged_item)
+                next if File.mtime(item) <= File.mtime(staged_item)
+            end
+            # TODO: add colour here (red)
+            puts "\t" + file_name
+        end
+    end
+
+end
+
+def diff(args)
+    # Check out https://github.com/samg/diffy as a gem to just do alla this for you.
 end
 
 def commit(args)
@@ -247,18 +291,29 @@ def commit(args)
         print("A commit message must be provided.")
         exit(0)
     end
-    author      = ENV['USER'] || ENV['USERNAME']
-    now         = DateTime.now.to_s
-    parent      = current_frame
-    commit_hash = Digest::SHA1.hexdigest(now + author + parent + message)
+    author     = ENV['USER'] || ENV['USERNAME']
+    now        = DateTime.now.to_s
+    parent     = current_frame
+    frame_name = Digest::SHA1.hexdigest(now + author + parent + message)
     
-    Dir.mkdir(path("frames", commit_hash))
-    Dir.mkdir(path("frames", commit_hash, ".frame"))
+    Dir.mkdir(vcr_path("frames", frame_name))
+    Dir.mkdir(vcr_path("frames", frame_name, ".frame"))
 
-    File.write(path("HEAD"), commit_hash)
+    File.write(vcr_path("HEAD"), frame_name)
 
-    create_frame(commit_hash)
-    # TODO: only copy staged files
+    source_path = vcr_path("staging")
+    target_path = vcr_path("frames", frame_name, ".frame")
+    Find.find(source_path) do |source|
+        target = source.sub(/^#{source_path}/, target_path)
+        # TODO: ignore files in a `.vcr-ignore` file as well.
+        if File.directory? source
+            Find.prune if File.basename(source) == '.vcr'
+            FileUtils.mkdir target unless File.exists? target
+        else
+            FileUtils.copy source, target
+        end
+    end
+    FileUtils.rm_rf(Dir.glob(vcr_path("staging", "*")))
 end
 
 def handle_command(command, args)
@@ -267,14 +322,18 @@ def handle_command(command, args)
         help(args)
     when "init"
         init(args)
-    when "add"
+    when "add", "stage"
         add(args)
+    when "rm", "unstage"
+        remove(args)
     when "track"
         track(args)
     when "commit"
         commit(args)
     when "status"
         status(args)
+    when "diff"
+        diff(args)
     when "checkout"
         checkout(args)
     when "log"
